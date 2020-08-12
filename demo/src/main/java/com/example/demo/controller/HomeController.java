84a75f6294b02b3d126ea9dc1ff6e3d4f3bb9a98
tako_eng <oys.sys@gmail.com>
2020-08-13 08:13:35 +0900

    Controllerにレコード一覧を取得する処理を追加（途中）

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.data.domain;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -22,7 +25,8 @@ public class HomeController {
     model.addAttribute("todo", new Todo());
     // ページのentityを取得する
     // https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/PagingAndSortingRepository.html?is-external=true#findAll-org.springframework.data.domain.Pageable-
-    model.addAttribute("allRecords", todoRepository.findAll());
+    Page<String> list = todoRepository.findAll();
+    model.addAttribute("allRecords", list);
     return "list"; // viewのファイル名を拡張子なしで記述する
   }
 
