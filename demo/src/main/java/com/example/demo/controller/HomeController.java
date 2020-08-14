0059417d12765e80d7016e52b29531ae9b25d6d1
tako_eng <oys.sys@gmail.com>
2020-08-14 11:40:26 +0900

    Controller修正中

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -10,8 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.data.domain;
+// import org.springframework.data.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -25,7 +26,7 @@ public class HomeController {
     model.addAttribute("todo", new Todo());
     // ページのentityを取得する
     // https://docs.spring.io/spring-data/commons/docs/current/api/org/springframework/data/repository/PagingAndSortingRepository.html?is-external=true#findAll-org.springframework.data.domain.Pageable-
-    Page<String> list = todoRepository.findAll();
+    ArrayList<Todo> list = (ArrayList) todoRepository.findAll();
     model.addAttribute("allRecords", list);
     return "list"; // viewのファイル名を拡張子なしで記述する
   }
